d777ae3a8666423ecc0a8f7a9abd3c56c78f6aa5
Cleverson Nahum <cleversonahum@gmail.com>
2018-02-26 18:55:09 -0300

    using vector instead of static arrays

--- aula1.cpp
+++ aula1.cpp
@@ -8,53 +8,55 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
+#define DEBUG 0 
+
 int main(int argc, char* argv[]) {
-	
-	if (argc != 3 || atoi(argv[2]) != atoi(argv[1])) {
-		cout << "Columns of the first matrix need to be equal the second matrix and number of parameters need to be equal 4";
+
+	if (argc != 2) {
+		cout << "Needs Matrix dimentions as a argument";
 		return -1;
 	}
-	
-	int r = atoi(argv[1]), c = atoi(argv[2]);
-	int m1[r][c], m2[r][c], mult[c][r];
+
+	int d = atoi(argv[1]);
+	vector<int> m1(d*d), m2(d*d), mult(d*d);
 
 	//Initializing Seed to random number
-        srand (time(0));
+	srand (time(0));
 
 	//Filling m1 and m2
-	for (int i = 0; i<r; i++) {
-		for (int j = 0; j<c; j++) {
-			m1[i][j] = rand() % 10 + 1;
-			m2[i][j] = rand() % 10 + 1;
-		}
+	for(int i =0; i<d*d; i++) {
+		m1[i] = rand() % 10 + 1;
+		m2[i] = rand() % 10 + 1;
 	}
 
-    //Filling mult
-    for (int i = 0; i<c; i++) {
-		for (int j = 0; j<r; j++) {
-			mult[i][j] = 0;
-		}
-	}
+
+	//Filling mult
+	for (int i = 0; i<d*d; i++) 
+		mult[i] = 0;
+	
 
 
 	// Multiplying matrix 1 and 2
-	for(int i = 0; i < r; ++i)
-		for(int j = 0; j < c; ++j)
-			for( int k = 0; k < c; ++k)
-	        {
-				mult[i][j] += m1[i][k] * m2[k][j];
-	        }
+	for(int i = 0; i < d; ++i)
+		for(int j = 0; j < d; ++j)
+			for( int k = 0; k < d; ++k)
+			{
+				mult[i*d+j] += m1[i*d+k] * m2[k*d+j];
+			}
+	
+	#if DEBUG==1
+	//Showing m1 and m2
+        for(int i = 0; i<d*d;i++)
+                cout<<"valor m1= "<<m1[i]<<"; valor m2= "<<m2[i]<<"\n";
 
-	//Showing matrix
-	//for (int i = 0; i<c1; i++) {:x
-	//
-	//	for (int j = 0; j<r2; j++) {
-	//		cout << mult[i][j]<<"teste";
-	//	}
-	//}
+	//Showing mult
+	for(int i = 0; i<d*d;i++)
+                cout<<"valor = "<<mult[i]<<"\n";
+	#endif
 
 	return 0;
 }
--- aula1.py
+++ aula1.py
@@ -3,20 +3,15 @@
 import sys
 import numpy as np
 
-if (len(sys.argv)!=3):
+if (len(sys.argv)!=2):
     print("ERROR: Insuficient Arguments")
     quit()
 
-if (sys.argv[2]!=sys.argv[1]): 
-    print("ERROR: Columns of the first matrix need to be equal the second matrix")
-    quit()
-
-r = int(sys.argv[1] )
-c = int(sys.argv[2])
+d = int(sys.argv[1])
 
 #Filling First Matrix Values
-m1 = np.random.randint(1,10,size=(r,c))
-m2 = np.random.randint(1,10,size=(r,c))
+m1 = np.random.randint(1,10,size=(d,d))
+m2 = np.random.randint(1,10,size=(d,d))
 #print(m1)
 #print(m2)
 mult = np.matmul(m1, m2)
