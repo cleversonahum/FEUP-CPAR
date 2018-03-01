6eacc9448188701b204bffa4626bfefd116b754b
Cleverson Nahum <cleversonahum@gmail.com>
2018-03-01 19:55:56 -0300

    Using PAPI, function C++ and JAVA updated and working

--- aula1.cpp
+++ aula1.cpp
@@ -6,8 +6,8 @@
  */
 
 #include <iostream>
-#include <stdlib.h>
 #include <vector>
+#include "papi.h"
 
 using namespace std;
 
@@ -15,8 +15,9 @@ using namespace std;
 
 //Protypes
 int multMatrix(int d);
-
+int mult2Matrix(int d);
 int multMatrixP(int d, int np);
+int mult2MatrixP(int d, int np);
 
 int main(int argc, char* argv[]) {
 
@@ -25,98 +26,206 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
+	//PAPI variables
+	float real_time, proc_time,ipc;
+	long long ins;
+	float real_time_i, proc_time_i, ipc_i;
+	long long ins_i;
+	int retval;
+
+	if((retval=PAPI_ipc(&real_time_i,&proc_time_i,&ins_i,&ipc_i)) < PAPI_OK)
+	{ 
+		printf("Could not initialise PAPI_ipc \n");
+		printf("retval: %d\n", retval);
+		exit(1);
+	}
+
 	switch (atoi(argv[1])) {
 		case 1:
 			multMatrix(atoi(argv[2]));
 			break;
 		case 2:
-			multMatrixP(atoi(argv[2]), atoi(argv[3]));
+			mult2Matrix(atoi(argv[2]));
 			break;
 		case 3:
-			cout<<"Function not implemented";
+			multMatrixP(atoi(argv[2]), atoi(argv[3]));
 			break;
 		case 4:
-			cout<<"Function not implemented";
+			mult2MatrixP(atoi(argv[2]), atoi(argv[3]));
 			break;
 	}
+
+	if((retval=PAPI_ipc( &real_time, &proc_time, &ins, &ipc))<PAPI_OK)
+	{    
+		printf("retval: %d\n", retval);
+		exit(1);
+	}
+
+
+	printf("Real_time: %f Proc_time: %f Total instructions: %lld IPC: %f\n", 
+			real_time, proc_time,ins,ipc);
+
+	/* clean up */
+	PAPI_shutdown();
 
 	return 0;
 }
 
 int multMatrix(int d) {
-        
-        vector<int> m1(d*d), m2(d*d), mult(d*d);
 
-        //Filling m1 and m2
-        for(int i =0; i<d*d; i++) {
-                m1[i] = i;
-                m2[i] = d*d-i;
-        }
+	vector<int> m1(d*d), m2(d*d), mult(d*d);
 
-
-        //Filling mult
-        for (int i = 0; i<d*d; i++)
-                mult[i] = 0;
+	//Filling m1 and m2
+	for(int i =0; i<d*d; i++) {
+		m1[i] = i+1;
+		m2[i] = d*d-i;
+	}
 
 
+	//Filling mult
+	for (int i = 0; i<d*d; i++)
+		mult[i] = 0;
 
-        // Multiplying matrix 1 and 2
-        for(int i = 0; i < d; ++i)
-                for(int j = 0; j < d; ++j)
-                        for( int k = 0; k < d; ++k)
-                        {
-                                mult[i*d+j] += m1[i*d+k] * m2[k*d+j];
-                        }
 
-        #if DEBUG==1
-        //Showing m1 and m2
-        for(int i = 0; i<d*d;i++)
-                cout<<"valor m1= "<<m1[i]<<"; valor m2= "<<m2[i]<<"\n";
 
-        //Showing mult
-        for(int i = 0; i<d*d;i++)
-                cout<<"valor = "<<mult[i]<<"\n";
-        #endif
+	// Multiplying matrix 1 and 2
+	for(int i = 0; i < d; ++i)
+		for(int j = 0; j < d; ++j){
+			for( int k = 0; k < d; ++k)
+			{
+				mult[i*d+j] += m1[i*d+k] * m2[k*d+j];
+			}
+		}
 
-        return 0;
+#if DEBUG==1
+	//Showing m1 and m2
+	for(int i = 0; i<d*d;i++)
+		cout<<"valor m1= "<<m1[i]<<"; valor m2= "<<m2[i]<<"\n";
+
+	//Showing mult
+	for(int i = 0; i<d*d;i++)
+		cout<<"valor = "<<mult[i]<<"\n";
+#endif
+
+	return 0;
+}
+
+int mult2Matrix(int d) {
+
+	vector<int> m1(d*d), m2(d*d), mult(d*d);
+
+	//Filling m1 and m2
+	for(int i =0; i<d*d; i++) {
+		m1[i] = i+1;
+		m2[i] = d*d-i;
+	}
+
+
+	//Filling mult
+	for (int i = 0; i<d*d; i++)
+		mult[i] = 0;
+
+
+
+	// Multiplying matrix 1 and 2
+	for(int i = 0; i < d; ++i)
+		for(int k = 0; k < d; ++k)
+			for( int j = 0; j < d; ++j)
+			{
+				mult[i*d+j] += m1[i*d+k] * m2[k*d+j];
+			}
+
+#if DEBUG==1
+	//Showing m1 and m2
+	for(int i = 0; i<d*d;i++)
+		cout<<"valor m1= "<<m1[i]<<"; valor m2= "<<m2[i]<<"\n";
+
+	//Showing mult
+	for(int i = 0; i<d*d;i++)
+		cout<<"valor = "<<mult[i]<<"\n";
+#endif
+
+	return 0;
 }
 
 int multMatrixP(int d, int nt) {
 
-        vector<int> m1(d*d), m2(d*d), mult(d*d);
+	vector<int> m1(d*d), m2(d*d), mult(d*d);
 	int i,j;
 
-        //Filling m1 and m2
-        for(int i =0; i<d*d; i++) {
-                m1[i] = i;
-                m2[i] = d*d-i;
-        }
+	//Filling m1 and m2
+	for(int i =0; i<d*d; i++) {
+		m1[i] = i+1;
+		m2[i] = d*d-i;
+	}
 
 
-        //Filling mult
-        for (int i = 0; i<d*d; i++)
-                mult[i] = 0;
+	//Filling mult
+	for (int i = 0; i<d*d; i++)
+		mult[i] = 0;
 
 
-	
-        // Multiplying matrix 1 and 2
-	#pragma omp parallel for private (i,j) num_threads (nt)
-        for(int i = 0; i < d; ++i)
-                for(int j = 0; j < d; ++j)
-                        for( int k = 0; k < d; ++k)
-                        {
-                                mult[i*d+j] += m1[i*d+k] * m2[k*d+j];
-                        }
 
-        #if DEBUG==1
-        //Showing m1 and m2
-        for(int i = 0; i<d*d;i++)
-                cout<<"valor m1= "<<m1[i]<<"; valor m2= "<<m2[i]<<"\n";
+	// Multiplying matrix 1 and 2
+#pragma omp parallel for private (i,j) num_threads (nt)
+	for(int i = 0; i < d; ++i)
+		for(int j = 0; j < d; ++j)
+			for( int k = 0; k < d; ++k)
+			{
+				mult[i*d+j] += m1[i*d+k] * m2[k*d+j];
+			}
 
-        //Showing mult
-        for(int i = 0; i<d*d;i++)
-                cout<<"valor = "<<mult[i]<<"\n";
-        #endif
+#if DEBUG==1
+	//Showing m1 and m2
+	for(int i = 0; i<d*d;i++)
+		cout<<"valor m1= "<<m1[i]<<"; valor m2= "<<m2[i]<<"\n";
 
-        return 0;
+	//Showing mult
+	for(int i = 0; i<d*d;i++)
+		cout<<"valor = "<<mult[i]<<"\n";
+#endif
+
+	return 0;
+}
+
+
+
+int mult2MatrixP(int d, int nt) {
+
+	vector<int> m1(d*d), m2(d*d), mult(d*d);
+	int i,j;
+
+	//Filling m1 and m2
+	for(int i =0; i<d*d; i++) {
+		m1[i] = i+1;
+		m2[i] = d*d-i;
+	}
+
+
+	//Filling mult
+	for (int i = 0; i<d*d; i++)
+		mult[i] = 0;
+
+
+
+	// Multiplying matrix 1 and 2
+#pragma omp parallel for private (i,j) num_threads (nt)
+	for(int i = 0; i < d; ++i)
+		for(int k = 0; k < d; ++k)
+			for( int j = 0; j < d; ++j)
+			{
+				mult[i*d+j] += m1[i*d+k] * m2[k*d+j];
+			}
+
+#if DEBUG==1
+	//Showing m1 and m2
+	for(int i = 0; i<d*d;i++)
+		cout<<"valor m1= "<<m1[i]<<"; valor m2= "<<m2[i]<<"\n";
+
+	//Showing mult
+	for(int i = 0; i<d*d;i++)
+		cout<<"valor = "<<mult[i]<<"\n";
+#endif
+
+	return 0;
 }
