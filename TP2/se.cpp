3ebd5674b2c642cf4d9a2b6fb5e8212cf441df5f
Cleverson Nahum <cleversonahum@gmail.com>
2018-04-16 18:18:50 -0300

    openmp implemented in se

--- TP2/se.cpp
+++ TP2/se.cpp
@@ -12,7 +12,30 @@ void se(int n, string filename) {
 	file.open(filename);
 	file << "Primes until " << n << "\n";
 
-	for (int p=2; p*p<=n; p++) {
+	for (int p=2; (p*p)<=n; p++) {
+		if (primes[p] == true) {
+        		for (int i=p*2; i<=n; i += p)
+			primes[i] = false;
+        	}
+	}
+ 
+	for (int p=2; p<=n; p++)
+		if (primes[p])
+			file << p << "\n";
+
+	file.close();
+}
+
+void se(int n, string filename, int nt) {
+	ofstream file; //CSV file
+	int sn = sqrt(n);
+	std::vector<bool> primes(n+1, true);
+
+	file.open(filename);
+	file << "Primes until " << n << "\n";
+
+#pragma omp parallel for num_threads(nt)
+	for (int p=2; p<=sn; p++) {
 		if (primes[p] == true) {
         		for (int i=p*2; i<=n; i += p)
 			primes[i] = false;
