5195d56eee66fba5b86fdf20da94e2dc02e1e252
Cleverson Nahum <cleversonahum@gmail.com>
2018-04-24 20:42:17 -0300

    se.cpp implemented using openmpi

--- TP2/se.cpp
+++ TP2/se.cpp
@@ -51,7 +51,7 @@ void se(int n, string filename, int nt) {
 	file.close();
 }
 
-void seP(int n, string filename, int argc, char* argv[]) {
+void se(int n, string filename, int argc, char* argv[]) {
 	int rank, size;
 	ofstream file; //CSV file
 	int sn = sqrt(n);
@@ -64,58 +64,47 @@ void seP(int n, string filename, int argc, char* argv[]) {
 	int ri= ceil(((float)sn-2.0)/(float)size);
 	int limit;
 
-	if(rank==0) {
-		cout<<"\nsn: "<<sn<<" size:"<<size;
-		cout<<"\nri: "<<ri<<"\n";
-	}
-
-	//file.open(filename);
-
-	//if(rank==0)
-	//	file << "Primes until " << n << "\n";
-
-	//Calculating multiples and marking array of primes, each processe will have a list of it multiples, so they are not prime numbers already, but they are numbers that are not multiples of the "looped" numbers
 	if(rank!=(size-1))
 		limit = ((2+rank*ri)+ri-1);
 	else
 		limit = sn;
 
-	cout<<"\nRank1, p= "<<(2+rank*ri)<<" until "<<limit<<"\n";
 	for (int p=(2+rank*ri); p<=limit; p++) {
-		cout<<"\nLoop "<<p<<"\n";
 		if (primes[p] == 1) {
 			for (int i=p*2; i<=n; i += p)
 				primes[i] = 0;
 		}
 	}
 
-	//Printing Primes Numbers
-	//cout<<"\nPrimes: \n";
-	//for (int i=2; i<=n; i++)
-	//	if(primes[i])
-	//		cout<<i<<" = "<<primes[i]<<endl;
-
 	MPI_Barrier(MPI_COMM_WORLD); //All process waits here until other process arrives in this barrier
-	cout<<"SAIU DA BARREIRA"<<endl;
-	if(rank==1) { //Sending primes array to process with rank 0
-		int number = rank;
-		MPI_Send(&number, 1, MPI_INT, 0, rank, MPI_COMM_WORLD);
+	if(rank!=0) { //Sending primes array to process with rank 0
+		MPI_Request status;
+		MPI_Isend(primes.data(), primes.size(), MPI_INT, 0, rank, MPI_COMM_WORLD, &status);
+	}
+	else { //Process 0
+		vector<vector<int>> number(size-1, vector<int>(n+1));
+		MPI_Request req[size-1];
+		MPI_Status status[size-1];
+
+		for(int i = 0; i<(size-1); i++) //Receiving from processes
+			MPI_Irecv(&number[i][0], (n+1), MPI_INT, (i+1), (i+1), MPI_COMM_WORLD, &req[i]);
+
+		for(int i = 0; i<(size-1); i++) //Waiting Received values to Number array
+			MPI_Wait(&req[i], &status[i]);
+
+		for(int j=0; j<(size-1);j++) //Comparing received values with primes generated
+			for(int i=0; i<primes.size();i++)
+				primes[i] &= number[j][i];
+
+		file.open(filename);
+        	file << "Primes until " << n << "\n";
+		if(rank==0)
+			for (int p=0; p<primes.size(); p++)
+				if (primes[p])
+					file << p << " ";
+		file.close();
 	}
 
-	//Receiving values calculated
-	if(rank==0) { //Process 1 is responsible for receive all processed matrixes. Receiving other matrixes of no multiples calculated in all process
-		//for(int i=1; i<size; i++) {
-			int teste;
-			MPI_Recv(&teste, 1, MPI_INT, rank, rank, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		//}
-		cout<<"\nTESTE: "<<teste<<endl;
-	}
-
-	//for (int p=2; p<=n; p++)
-	//	if (primes[p])
-	//		file << p << " ";
-
-	//file.close();
 
 	MPI_Finalize();
 }
@@ -126,6 +115,6 @@ int main(int argc, char* argv[]) {
 	int n = atoi(argv[1]);
 	string filename = argv[2];
 	//int nt = atoi(argv[3]);
-	seP(atoi(argv[1]), argv[2], argc, argv);
+	se(atoi(argv[1]), argv[2], argc, argv);
 	return 0;
 }
