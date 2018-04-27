6061e6a8158ccace39c261dd351f1d7272c889c5
Cleverson Nahum <cleversonahum@gmail.com>
2018-04-27 17:13:56 -0300

    removing old fuunction lu mpi dDoolitle

--- TP2/lu.cpp
+++ TP2/lu.cpp
@@ -120,112 +120,7 @@ void lu(vector<vector<double>> mat, int n, int nt) {
 	l.close();
 }
 
-//void lu(vector<vector<double>> mat, int n, int argc, char* argv[]) {
-
-	//vector<double> lower(n*n), upper(n*n);
-	//ofstream l, u;
-	//int rank, size;
-
-	//MPI_Init(&argc, &argv);
-	//MPI_Comm_size(MPI_COMM_WORLD, &size);
-	//MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-	//int limit, ri;
-	//ri = ceil(((float)n)/(float)size);
-
-	//if(rank!=(size-1)) //Defining Limits for Loops
-		//limit = (rank*ri)+ri;
-	//else
-		//limit = n;
-
-	//for (int i = (rank*ri); i < limit; i++) { //Calculating upper and lower matrix
-		//// Upper
-		//for (int k = i; k < n; k++) {
-			//// Sum L(i, j) * U(j, k)
-			//int sum = 0;
-			//for (int j = 0; j < i; j++)
-				//sum += (lower[i*n+j] * upper[j*n+k]);
-
-			//// U(i, k)
-			//upper[i*n+k] = mat[i][k] - sum;
-		//}
-
-
-		//// Lower 
-		//for (int k = i; k < n; k++) {
-			//if (i == k)
-				//lower[i*n+i] = 1; // Diagonal
-			//else {
-
-				//// Sum L(k, j) * U(j, i)
-				//int sum = 0;
-				//for (int j = 0; j < i; j++)
-					//sum += (lower[k*n+j] * upper[j*n+i]);
-
-				//// L(k, i)
-				//lower[k*n+i] = (mat[k][i] - sum) / upper[i*n+i];
-			//}
-		//}
-
-	//}
-
-	//MPI_Barrier(MPI_COMM_WORLD);
-
-	//if(rank!=0) { //Sending array to process with rank 0
-		//MPI_Request req[2];
-		//MPI_Isend(upper.data(), upper.size(), MPI_DOUBLE, 0, rank*rank, MPI_COMM_WORLD, &req[0]);
-		//MPI_Isend(lower.data(), lower.size(), MPI_DOUBLE, 0, rank*rank+1, MPI_COMM_WORLD, &req[1]);
-	//}
-	//else { //Process 0
-		//vector<vector<double>> received(size-1, vector<double>(n*n));
-		//MPI_Request req[(size-1)*2];
-		//MPI_Status status[(size-1)*2];
-		//int cont = 1;
-
-		//for(int i=0; i<(size-1); i++) { //Receiving from process {
-			//cout<<"CONT "<<cont<<endl;
-			//MPI_Irecv(&received[i][0], n*n, MPI_DOUBLE, i+1, (i+1)*(i+1), MPI_COMM_WORLD, &req[i*cont]);
-			//MPI_Irecv(&received[i][0], n*n, MPI_DOUBLE, i+1, (i+1)*(i+1)+1, MPI_COMM_WORLD, &req[i*cont+1]);
-			//if(i!=(size-3))
-				//cont++;
-		//}
-
-		//for(int i = 0; i<2*(size-1);i++) //Waiting received values
-			//MPI_Wait(&req[i], &status[i]);
-
-		////DO MAGIC HERE
-		//for(int i=0; i<size-1; i++) 
-			//for(int j=0;j<n*n;j++)
-				//if(j==(n*n-1))
-					//cout<<received[i][j]<<endl;
-				//else
-					//cout<<received[i][j]<<" ";
-
-
-		////Writing CSV
-		//u.open("U.csv");
-		//l.open("L.csv");
-		//for (int i = 0; i < n; i++) {
-			//// Lower
-			//for (int j = 0; j < n; j++)
-				//l << lower[i*n+j] << "\t"; 
-
-			//// Upper
-			//for (int j = 0; j < n; j++)
-				//u << upper[i*n+j] << "\t";
-			//u << endl;
-			//l << endl;
-		//}
-
-		//u.close();
-		//l.close();
-
-		//}
-
-		//MPI_Finalize();
-	//}
-
-void lu(vector <float> mat, int n, int argc, char* argv[]) { //Usian Forward Gauss Elimination
+void lu(vector <float> mat, int n, int argc, char* argv[]) { //Using Forward Gauss Elimination
 	int size, rank;
 	MPI_Init(NULL, NULL);
 	MPI_Comm_size(MPI_COMM_WORLD, &size); 
@@ -290,7 +185,7 @@ void lu(vector <float> mat, int n, int argc, char* argv[]) { //Usian Forward Gau
 }
 
 
-int readCSV(string csvFile, vector<vector<double>> &mat, int d) {
+int readCSV(string csvFile, vector<vector<double>> &mat, int d) { //stores in a bidimentional array
 
 	std::ifstream file(csvFile);
 
@@ -316,7 +211,7 @@ int readCSV(string csvFile, vector<vector<double>> &mat, int d) {
 	file.close();
 }
 
-int readCSVmpi(string csvFile, vector<float> &mat, int d) {
+int readCSVmpi(string csvFile, vector<float> &mat, int d) { //stores in a unidimentional array
 
 	std::ifstream file(csvFile);
 
